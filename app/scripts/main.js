1915995c6f5472350d79e0d581553b3e8d95eb33
Dillon Fosa <dillonfosa@gmail.com>
2017-04-13 21:08:10 -0400

    added gallery and updated main landing page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,35 @@
-$( document ).ready(function(){
-    
-    $('.button-collapse').sideNav();
+$(document).ready(function () {
+    $('.button-collapse').sideNav({
+        menuWidth: 400, // Default is 240
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true
+    });
+    new WOW().init();
+    echo.init({
+        offset: 100
+        , throttle: 250
+        , unload: false
+        , callback: function (element, op) {
+            console.log(element, 'has been', op + 'ed')
+        }
+    });
     
 });
 
-
-
-
+$(document).ready(function ($) {
+        $('.image img').click(function (event) {
+            // detect data-id for later
+            var id = $(this).data('id');
+            // grab src to replace #featured
+            var src = $(this).attr('src');
+            // set featured image
+            var img = $('#featured img');
+            img.fadeOut('fast', function () {
+                $(this).attr({
+                    src: src
+                , });
+                $(this).fadeIn('fast');
+            });
+        });
+    });
 

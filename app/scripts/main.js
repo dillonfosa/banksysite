fe4d4c1678a6bafc624f47d9fe5327b9e6594ab3
Dillon Fosa <dillonfosa@gmail.com>
2017-04-30 12:13:19 -0400

    added correct fix for img sizing in lightbox

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -73,10 +73,10 @@ $(document).ready(function(){
 
 $(document).ready(function(){
   // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+  $('a').on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+    if (this.hash !== '') {
       // Prevent default anchor click behavior
       event.preventDefault();
 
